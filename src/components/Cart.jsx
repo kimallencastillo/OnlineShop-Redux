322a1ce7242c4b68860ebbd57bffdde23d9ff56f
kimallencastillo <kimallencastillo@gmail.com>
2023-04-10 22:24:44 +0800

    design shopping cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
+  //₱
   const cart = useSelector((state) => state.cart);
   return (
     <div className="cart-container">
@@ -30,16 +31,67 @@ const Cart = () => {
         </div>
       ) : (
         <div>
-          <div className="title">
+          <div className="titles">
             <h3 className="product-title">Product</h3>
             <h3 className="price">Price</h3>
-            <h3 className="Quantity">Quantity</h3>
+            <h3 className="quantity">Quantity</h3>
             <h3 className="total">Total</h3>
           </div>
-          <div children="cart-items">
+          {/* Cart Item */}
+          <div className="cart-items">
             {cart.cartItems?.map((cartItem) => {
-              <div className="cart-item" ket={cartItem.id}></div>;
+              return (
+                <div className="cart-item" key={cartItem.id}>
+                  <div className="cart-product">
+                    <img src={cartItem.image} alt={cartItem.name} />
+                    <div>
+                      <h3>{cartItem.name}</h3>
+
+                      <button>Remove</button>
+                    </div>
+                  </div>
+                  <div className="cart-product-price">${cartItem.price}</div>
+                  <div className="cart-product-quantity">
+                    <button>-</button>
+                    <div className="count">{cartItem.cartQuantity}</div>
+                    <button>+</button>
+                  </div>
+                  <div className="cart-product-total-price">
+                    ${cartItem.price * cartItem.cartQuantity}
+                  </div>
+                </div>
+              );
             })}
+            {/* Cart Summary */}
+            <div className="cart-summary">
+              <button className="clear-cart">Clear Cart</button>
+              <div className="cart-checkout">
+                <div className="subtotal">
+                  <span>Subtotal</span>
+                  <span className="amount">${cart.cartTotalAmount}</span>
+                </div>
+                <p>Taxes and shipping calculated at checkout</p>
+                <button>Check Out</button>
+                <div className="continue-shopping">
+                  <Link to="/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-arrow-left-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                      />
+                    </svg>
+                    <span>Continue Shopping</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       )}
