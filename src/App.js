7545e81e1d1fb61cd8fbeba889d95e8366e36b3a
kimallencastillo <kimallencastillo@gmail.com>
2023-04-18 20:44:27 +0800

    productDetails

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import Details from "./components/Details";
 import Loading from "./templates/loading";
+import ProductDetails from "./templates/productDetails";
 function App() {
   return (
     <div className="App">
@@ -22,6 +23,7 @@ function App() {
           <Route path="/not-found" element={<NotFound />} />
           <Route path="/details/:productId" element={<Details />} />
           <Route path="/loading" element={<Loading />} />
+          <Route path="productDetails" element={<ProductDetails />} />
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
